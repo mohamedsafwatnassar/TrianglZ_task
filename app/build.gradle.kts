1c1905d841247eda3d2fdb84f6b38fe31267f26b
mohamedsafwatnassar <mohamed.nasar8710@gmail.com>
2026-02-13 16:13:10 +0200

    Fix compileSdk configuration issue in app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,14 +9,12 @@ plugins {
 
 android {
     namespace = "com.trianglz.chatapp"
-    compileSdk {
-        version = release(34)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.trianglz.chatapp"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
